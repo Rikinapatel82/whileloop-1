e95227cafad36ade6b4d06a7dc31148f007b5f1c
Rikinapatel82 <149239614+Rikinapatel82@users.noreply.github.com>
2023-11-20 19:43:37 +0000

    Add files via upload

---
+++ EvenNumber.java
@@ -0,0 +1,16 @@
+package evennumberwhileloop;
+
+public class EvenNumber {
+    public static void main(String args[]) {
+        int i= 40;
+        while (i>=1) {
+            if (i >= 1) ;{
+                System.out.println( i +    " EVEN NUMBER" );  i=i-1;{
+
+            }
+                System.out.println(  i +    " ODD NUMBER" );  i=i-1;
+            }
+        }
+    }
+}
+
---
+++ WhileLoopTable9.java
@@ -0,0 +1,11 @@
+package table9;
+
+public class WhileLoopTable9 {
+    public static void main(String[] args) {
+        int a=1;
+        int myNotable =9;
+        while (a<=20){
+            System.out.printf("%d * %d = %d ", myNotable , a , myNotable * a); a++;
+        }
+    }
+}
